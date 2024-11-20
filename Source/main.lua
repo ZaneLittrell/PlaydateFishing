4bbeabf4ca8d3ba78988edad6d4064ef17893406
Zane Littrell <littrellzane@gmail.com>
2024-11-20 19:17:23 +0000

    Move images to subdirectory.

--- Source/main.lua
+++ Source/main.lua
@@ -9,28 +9,56 @@ local BACKGROUND_TAG <const> = 0xFF
 -- Playdate sprite object
 local pSprite <const> = playdate.graphics.sprite
 
--- Initialization function
-local function init()
-    -- Load wave animation
-    local waveTable = util.loadImagetable("fullWave")
-    local beachSprite = beach.beachSprite(waveTable)
-    beachSprite:add()
+-- Load all assets
+local function loadAssets()
+    local waveTable = util.loadImagetable("Images/fullWave")
+    local fishImage = util.loadImage("Images/fish")
+    local playerImage = util.loadImage("Images/player")
 
-    -- Make fish sprites
-    local fishImg = util.loadImage("fish")
+    return {
+        waveTable = waveTable,
+        fishImage = fishImage,
+        playerImage = playerImage
+    }
+end
+
+-- Make and add beach sprite
+local function initBeach(assets)
+    local beachSprite = beach.beachSprite(assets.waveTable)
+    beachSprite:add()
+end
+
+-- Make and add fish sprites
+local function initFish(assets)
     for i = 1, 3, 1 do
         local x = math.random(80, 320)
         local leftBound = math.random(50, 120)
         local rightBound = math.random(200, 350)
         local speed = math.random(25, 100) / 100
-        local sprite = fish.fishSprite(fishImg, x, i * 20, leftBound, rightBound, speed)
+        local sprite = fish.fishSprite(
+            assets.fishImage,
+            x,
+            i * 20,
+            leftBound,
+            rightBound,
+            speed
+        )
         sprite:add()
     end
+end
 
-    -- Make player sprite
-    local playerImg = util.loadImage("player")
-    local playerSprite = player.playerSprite(playerImg, 192, 200)
+-- Make and add player sprite
+local function initPlayer(assets)
+    local playerSprite = player.playerSprite(assets.playerImage, 192, 200)
     playerSprite:add()
+end
+
+-- Initialization function
+local function init()
+    local assets = loadAssets()
+    initBeach(assets)
+    initFish(assets)
+    initPlayer(assets)
 end
 
 --#region Playdate overrides
